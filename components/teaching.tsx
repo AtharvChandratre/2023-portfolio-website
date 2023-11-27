88bc8eb893ce3c53f9a4a3121a4acbf43c1932c1
Atharv Chandratre <atharvchandratre@gmail.com>
2023-11-27 10:26:52 -0600

    Fixing Teaching Link in NavBar

--- components/teaching.tsx
+++ components/teaching.tsx
@@ -10,7 +10,7 @@ export default function Teaching() {
     const { ref } = useSectionInView("Teaching", 0.5);
 
     return (
-        <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+        <section ref={ref} id="teaching" className="scroll-mt-28 mb-28">
             <SectionHeading>My Teaching Experience</SectionHeading>
             <div className="grid lg:grid-cols-3 gap-4 grid-cols-1">
                 {teachingData.map((project, index) => (
